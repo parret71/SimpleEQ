eeb18cb3096276be65bf3b0f96f1cc91aa4267e9
Seungtak Han <sqwvplus@naver.com>
2023-09-19 19:30:12 +0900

    DRY refactored DSP filter configuration

--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -130,6 +130,12 @@ private:
         }
         }
     }
+
+    void updateLowCutFilters(const ChainSettings& chainSettings);
+    void updateHighCutFilters(const ChainSettings& chainSettings);
+
+    void updateFilters();
+
     //==============================================================================
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (SimpleEQAudioProcessor)
 };
